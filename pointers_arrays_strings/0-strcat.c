ed6188e296ea162bf9f6d42f9cdd43b240c3906f
raycespencer <raycespen@gmail.com>
2024-06-03 18:45:56 +0000

    magic

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,18 @@
-#include "atlas.h"
-
-/**
- * _strcat - Concatenates two strings
- * @dest: The destination string
- * @src: The source string
- *
- * Return: A pointer to the resulting string dest
- */
 char *_strcat(char *dest, char *src)
 {
-    int dest_len = 0;
-    int i;
+    while (*dest != '\0')
+    {
+        dest++;
+    }
 
-    while (dest[dest_len] != '\0')
-        dest_len++;
+    while (*src != '\0')
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
 
-    for (i = 0; src[i] != '\0'; i++)
-        dest[dest_len++] = src[i];
-
-    dest[dest_len] = '\0';
+    *dest = '\0';
 
     return dest;
 }
