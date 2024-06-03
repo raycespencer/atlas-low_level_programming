7248c0b5c91f0fd136abdeacee23ba43d5bc85a0
raycespencer <raycespen@gmail.com>
2024-06-03 18:42:41 +0000

    concatenates two strings

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,30 +1,18 @@
-#include <stdio.h>
+char *_strcat(char *dest, char *src)
+{
+    while (*dest != '\0')
+    {
+        dest++;
+    }
 
-char *_strcat(char *dest, const char *src) {
-	char *dest_start = dest;
+    while (*src != '\0')
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
 
+    *dest = '\0';
 
-	while (*dest != '/0') {
-		dest++;
-	}
-
-	while (src != '/0') {
-		*dest = *src;
-		dest++;
-		src++;
-	}
-
-	dest = '/0';
-
-	return dest_start;
-}
-
-int main() {
-	char dest[100] = "hello, ";
-	char src{} = "World!";
-
-	_strcat(dest, src);
-	printf("%s/n", dest);
-
-	return 0;
+    return dest;
 }
