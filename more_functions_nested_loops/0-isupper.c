8ddb834e837f2449a5625ab478a09a5df96552d8
raycespencer <raycespen@gmail.com>
2024-06-04 16:44:54 +0000

    case check

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - check if letter is uppercase
+ * @x: The number to check
+ * Return: 1 for uppercase or 0 for other
+ */
+
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return(1);
+	}
+	return (0);
+}	
